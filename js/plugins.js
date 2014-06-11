d4db8821fb8147264d017ab7ef676a01dd9c0f94
Alex Lea <jkdalex@gmail.com>
2014-06-11 15:36:36 -0700

    Plugin rewrite

--- js/plugins.js
+++ js/plugins.js
@@ -30,37 +30,46 @@ var inArray = function(needle, haystack) {
 	return false;
 };
 
-// Place any jQuery/helper plugins in here.]
-$('td[data-key]').on('click', function(){
-	$('.highlight').removeClass('highlight');
-	var player = $(this).attr('data-key');
-	$('[data-key='+player+']').addClass('highlight');
-});
-
-// Validate entries
-$('tr[data-key]').each(function(){
-	var self = $(this),
-		player = self.attr('data-key'),
-		expectedGames = parseInt(self.find('.player span').html()),
-		matches = [];
-
-	// Get own matches
-	self.find('td[data-key]').each(function(i){
-		if ( i > 0 ) {
-			matches.push($(this).attr('data-key'));
-		}
+$(document).ready(function(){
+	// Place any jQuery/helper plugins in here.]
+	$('td[data-key]').on('click', function(){
+		$('.highlight').removeClass('highlight');
+		var player = $(this).attr('data-key');
+		$('[data-key='+player+']').addClass('highlight');
 	});
 
-	// Get individual matches
-	$('td[data-key='+player+']').each(function(){
-		var parent = $(this).parent('tr[data-key]').attr('data-key');
-		if ( parent != player && !inArray(parent, matches) ) {
-			matches.push(parent);
-		}
+	// Validate entries
+	var matchReady = 'matchReady';
+
+	$('body').on(matchReady, function(){
+		$('tr[data-key]').each(function(){
+			var self = $(this),
+				player = self.attr('data-key'),
+				expectedGames = parseInt(self.find('.player span').html()),
+				matches = [];
+
+			// Get own matches
+			self.find('td[data-key]').each(function(i){
+				if ( i > 0 ) {
+					matches.push($(this).attr('data-key'));
+				}
+			});
+
+			// Get individual matches
+			$('td[data-key='+player+']').each(function(){
+				var parent = $(this).parent('tr[data-key]').attr('data-key');
+				if ( parent != player && !inArray(parent, matches) ) {
+					matches.push(parent);
+				}
+			});
+
+			// Mark invalid ones
+			if ( expectedGames != matches.length ) {
+				console.log(player+'('+expectedGames+'): '+matches+'('+matches.length+')');
+			}
+		});
 	});
 
-	// Mark invalid ones
-	if ( expectedGames != matches.length ) {
-		console.log(player+'('+expectedGames+'): '+matches+'('+matches.length+')');
-	}
+	// Trigger body
+	$('body').trigger(matchReady);
 });
